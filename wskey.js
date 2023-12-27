b66905ccca8a5c08b0a4648d432bacf0213e643b
iBlackqq <86874908+iBlackqq@users.noreply.github.com>
2023-12-27 19:04:35 +0800

    wskey.js

---
+++ wskey.js
@@ -0,0 +1,19 @@
+
+/**
+ * @fileoverview Example to compose HTTP request
+ * and handle the response.
+ *
+ */
+
+ck =$request.headers.Cookie
+var CookieValue = ck.match(/wskey=.+?;/)
+
+data=JSON.parse($response.body)
+    data = data.userInfoSns.unickName
+    url = 'iBlack,wskey%E8%8E%B7%E5%8F%96%E6%8F%90%E9%86%92%E6%82%A8'
+
+
+
+console.log(ck)
+$notify(decodeURI(url), "京东wskey获取成功", "pin="+data+";"+CookieValue);
+    $done();
